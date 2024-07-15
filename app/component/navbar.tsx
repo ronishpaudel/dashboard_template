5141c522cf8598653917b873161d2a7d8a44ecea
robuGdada <paudelronish@gmail.com>
2024-07-15 19:01:40 +0545

    responsive nabar and navabr add

---
+++ app/component/navbar.tsx
@@ -0,0 +1,43 @@
+import React from "react";
+import Link from "next/link";
+import {
+  Package2Icon,
+  HomeIcon,
+  ShoppingCartIcon,
+  PackageIcon,
+  UsersIcon,
+  LineChartIcon,
+} from "lucide-react";
+const NavBar = () => {
+  return (
+    <nav className="grid gap-6 text-lg font-medium">
+      <Link
+        href="#"
+        className="group flex h-10 w-10 shrink-0 items-center justify-center gap-2 rounded-full bg-primary text-lg font-semibold text-primary-foreground md:text-base"
+        prefetch={false}
+      >
+        <Package2Icon className="h-5 w-5 transition-all group-hover:scale-110" />
+        <span className="sr-only">Acme Inc</span>
+      </Link>
+      <Link
+        href="/dashboard"
+        className="flex items-center gap-4 px-2.5 text-muted-foreground hover:text-foreground"
+        prefetch={false}
+      >
+        <HomeIcon className="h-5 w-5" />
+        Dashboard
+      </Link>
+
+      <Link
+        href="/dahsboard/add-product"
+        className="flex items-center gap-4 px-2.5 text-muted-foreground hover:text-foreground"
+        prefetch={false}
+      >
+        <PackageIcon className="h-5 w-5" />
+        Products
+      </Link>
+    </nav>
+  );
+};
+
+export default NavBar;
---
+++ app/component/side-nabar.tsx
@@ -0,0 +1,64 @@
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
+import {
+  Package2Icon,
+  HomeIcon,
+  ShoppingCartIcon,
+  PackageIcon,
+  UsersIcon,
+  LineChartIcon,
+  SettingsIcon,
+} from "lucide-react";
+import React from "react";
+import Link from "next/link";
+
+const SideNavBar = () => {
+  return (
+    <aside className="fixed inset-y-0 left-0 z-10 hidden w-14 flex-col border-r bg-background sm:flex">
+      <nav className="flex flex-col items-center gap-4 px-2 sm:py-5">
+        <TooltipProvider>
+          <Link
+            href="#"
+            className="group flex h-9 w-9 shrink-0 items-center justify-center gap-2 rounded-full bg-primary text-lg font-semibold text-primary-foreground md:h-8 md:w-8 md:text-base"
+            prefetch={false}
+          >
+            <Package2Icon className="h-4 w-4 transition-all group-hover:scale-110" />
+            <span className="sr-only">Acme Inc</span>
+          </Link>
+          <Tooltip>
+            <TooltipTrigger asChild>
+              <Link
+                href="/dashboard"
+                className="flex h-9 w-9 items-center justify-center rounded-lg bg-accent text-accent-foreground transition-colors hover:text-foreground md:h-8 md:w-8"
+                prefetch={false}
+              >
+                <HomeIcon className="h-5 w-5" />
+                <span className="sr-only">Dashboard</span>
+              </Link>
+            </TooltipTrigger>
+            <TooltipContent side="right">Dashboard</TooltipContent>
+          </Tooltip>
+          <Tooltip>
+            <TooltipTrigger asChild>
+              <Link
+                href="/dashboard/add-product"
+                className="flex h-9 w-9 items-center justify-center rounded-lg text-muted-foreground transition-colors hover:text-foreground md:h-8 md:w-8"
+                prefetch={false}
+              >
+                <PackageIcon className="h-5 w-5" />
+                <span className="sr-only">Products</span>
+              </Link>
+            </TooltipTrigger>
+            <TooltipContent side="right">Products</TooltipContent>
+          </Tooltip>
+        </TooltipProvider>
+      </nav>
+    </aside>
+  );
+};
+
+export { SideNavBar };

2190cc34a439dd7124bc85d4658b692577bbf9cd
robuGdada <paudelronish@gmail.com>
2024-08-08 21:24:39 +0545

    ggogle oauth [provider aded

---
+++ app/component/oAuth.tsx
@@ -0,0 +1,53 @@
+"use client";
+
+import React from "react";
+import {
+  CredentialResponse,
+  GoogleLogin,
+  GoogleOAuthProvider,
+} from "@react-oauth/google";
+import useAuth from "./hooks/postHooks/useAuth";
+
+const OAuth: React.FC = () => {
+  const { mutate: login, isError, error } = useAuth();
+
+  const handleLoginSuccess = (response: CredentialResponse) => {
+    console.log("Login Success:", response);
+    const token = response.credential!;
+    login(token);
+  };
+
+  const handleLoginFailure = () => {
+    console.error("Login Failed");
+  };
+
+  return (
+    <GoogleOAuthProvider clientId={process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID!}>
+      <div className="flex flex-col items-center justify-center w-full h-[100vh]">
+        <h1 className="text-2xl font-bold dark:text-white">
+          Sign in to your account
+        </h1>
+        <p className="mt-2 text-gray-600 dark:text-gray-300">
+          Or register for a new account
+        </p>
+
+        {isError && (
+          <div className="mt-4 text-red-500">
+            {error instanceof Error
+              ? error.message
+              : "Login failed. Please try again."}
+          </div>
+        )}
+
+        <div className="mt-4">
+          <GoogleLogin
+            onSuccess={handleLoginSuccess}
+            onError={handleLoginFailure}
+          />
+        </div>
+      </div>
+    </GoogleOAuthProvider>
+  );
+};
+
+export { OAuth };

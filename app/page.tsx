2726ee8f53ed004cdd63d7cd4fd1e2990566b046
robuGdada <paudelronish@gmail.com>
2024-08-08 21:26:13 +0545

    refactir landing page

--- app/page.tsx
+++ app/page.tsx
@@ -1,36 +1,15 @@
 "use client";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import React from "react";
-import { GoogleLogin, GoogleOAuthProvider } from "@react-oauth/google";
+import { OAuth } from "./component/oAuth";
 
 const Home: React.FC = () => {
-  const handleLoginSuccess = (response: any) => {
-    console.log("Login Success:", response);
-  };
+  const queryClient = new QueryClient();
 
-  const handleLoginFailure = () => {
-    console.error("Login Failed");
-  };
-  console.log(process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID!);
   return (
-    <GoogleOAuthProvider clientId={process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID!}>
-      <div className="flex flex-col items-center justify-center w-full h-[100vh]">
-        <h1 className="text-2xl font-bold dark:text-white">
-          Sign in to your account
-        </h1>
-        <p className="mt-2 text-gray-600 dark:text-gray-300 ">
-          Or register for a new account
-        </p>
-        <p className="mt-2 text-gray-600 dark:text-gray-300">
-          in the new world ecommerce world
-        </p>
-        <div className="mt-4">
-          <GoogleLogin
-            onSuccess={handleLoginSuccess}
-            onError={handleLoginFailure}
-          />
-        </div>
-      </div>
-    </GoogleOAuthProvider>
+    <QueryClientProvider client={queryClient}>
+      <OAuth />
+    </QueryClientProvider>
   );
 };
 

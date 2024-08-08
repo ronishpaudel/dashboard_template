15175eace4624b54e6e412464ce8f166307f4abc
robuGdada <paudelronish@gmail.com>
2024-08-08 21:25:39 +0545

    dashboardd layout fix

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,26 +1,21 @@
 "use client";
 
 import { ThemeProvider } from "../component/theme-provider";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-
 export default function DashboardLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  const queryClient = new QueryClient();
   return (
     <>
-      <QueryClientProvider client={queryClient}>
-        <ThemeProvider
-          attribute="class"
-          defaultTheme="system"
-          enableSystem
-          disableTransitionOnChange
-        >
-          <div className="flex h-screen ">{children}</div>
-        </ThemeProvider>
-      </QueryClientProvider>
+      <ThemeProvider
+        attribute="class"
+        defaultTheme="system"
+        enableSystem
+        disableTransitionOnChange
+      >
+        <div className="flex h-screen ">{children}</div>
+      </ThemeProvider>
     </>
   );
 }

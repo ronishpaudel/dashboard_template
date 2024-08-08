205956a0dcfce475998aa382c2a28582f0140046
robuGdada <paudelronish@gmail.com>
2024-08-08 21:25:10 +0545

    hoc authenticate route only

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,48 +1,10 @@
-import Link from "next/link";
-import { Sheet, SheetTrigger, SheetContent } from "@/components/ui/sheet";
-import { Button } from "@/components/ui/button";
-import {
-  Breadcrumb,
-  BreadcrumbList,
-  BreadcrumbItem,
-  BreadcrumbLink,
-  BreadcrumbSeparator,
-  BreadcrumbPage,
-} from "@/components/ui/breadcrumb";
-
-import {
-  DropdownMenu,
-  DropdownMenuTrigger,
-  DropdownMenuContent,
-  DropdownMenuLabel,
-  DropdownMenuSeparator,
-  DropdownMenuItem,
-} from "@/components/ui/dropdown-menu";
-import {
-  Card,
-  CardHeader,
-  CardTitle,
-  CardDescription,
-  CardContent,
-} from "@/components/ui/card";
-import { Tabs, TabsContent } from "@/components/ui/tabs";
-import {
-  Table,
-  TableHeader,
-  TableRow,
-  TableHead,
-  TableBody,
-  TableCell,
-} from "@/components/ui/table";
-import { Badge } from "@/components/ui/badge";
-import { PanelLeftIcon } from "lucide-react";
+"use client";
 import { SideNavBar } from "../component/side-nabar";
-import NavBar from "../component/navbar";
-import { ModeToggle } from "../component/toggleBtn";
 import Header from "../component/header";
 import { Main } from "../component/main";
+import withAuth from "@/utils/withAuth";
 
-export default function Component() {
+function page() {
   return (
     <div className="flex min-h-screen w-full flex-col bg-muted/40">
       <SideNavBar />
@@ -53,3 +15,5 @@ export default function Component() {
     </div>
   );
 }
+
+export default withAuth(page);

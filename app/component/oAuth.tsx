d1ad497119052963c13e6a950f1642522726667e
robuGdada <paudelronish@gmail.com>
2024-08-09 21:26:21 +0545

    oauth page fix

--- app/component/oAuth.tsx
+++ app/component/oAuth.tsx
@@ -22,31 +22,29 @@ const OAuth: React.FC = () => {
   };
 
   return (
-    <GoogleOAuthProvider clientId={process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID!}>
-      <div className="flex flex-col items-center justify-center w-full h-[100vh]">
-        <h1 className="text-2xl font-bold dark:text-white">
-          Sign in to your account
-        </h1>
-        <p className="mt-2 text-gray-600 dark:text-gray-300">
-          Or register for a new account
-        </p>
+    <div className="flex flex-col items-center justify-center w-full h-[100vh]">
+      <h1 className="text-2xl font-bold dark:text-white">
+        Sign in to your account
+      </h1>
+      <p className="mt-2 text-gray-600 dark:text-gray-300">
+        Or register for a new account
+      </p>
 
-        {isError && (
-          <div className="mt-4 text-red-500">
-            {error instanceof Error
-              ? error.message
-              : "Login failed. Please try again."}
-          </div>
-        )}
-
-        <div className="mt-4">
-          <GoogleLogin
-            onSuccess={handleLoginSuccess}
-            onError={handleLoginFailure}
-          />
+      {isError && (
+        <div className="mt-4 text-red-500">
+          {error instanceof Error
+            ? error.message
+            : "Login failed. Please try again."}
         </div>
+      )}
+
+      <div className="mt-4">
+        <GoogleLogin
+          onSuccess={handleLoginSuccess}
+          onError={handleLoginFailure}
+        />
       </div>
-    </GoogleOAuthProvider>
+    </div>
   );
 };
 

b16a77702c68afabb052584d49497e95e38973d5
robuGdada <paudelronish@gmail.com>
2024-08-09 21:25:58 +0545

    fuxx

--- app/page.tsx
+++ app/page.tsx
@@ -2,13 +2,16 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import React from "react";
 import { OAuth } from "./component/oAuth";
+import { GoogleOAuthProvider } from "@react-oauth/google";
 
 const Home: React.FC = () => {
   const queryClient = new QueryClient();
 
   return (
     <QueryClientProvider client={queryClient}>
-      <OAuth />
+      <GoogleOAuthProvider clientId={process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID!}>
+        <OAuth />
+      </GoogleOAuthProvider>
     </QueryClientProvider>
   );
 };

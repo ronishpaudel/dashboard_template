831c02be6abdfc9cfb1886c3004534eb99263dc0
robuGdada <paudelronish@gmail.com>
2024-08-10 20:26:27 +0545

    fix goofle auth

--- app/page.tsx
+++ app/page.tsx
@@ -2,16 +2,13 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import React from "react";
 import { OAuth } from "./component/oAuth";
-import { GoogleOAuthProvider } from "@react-oauth/google";
 
 const Home: React.FC = () => {
   const queryClient = new QueryClient();
 
   return (
     <QueryClientProvider client={queryClient}>
-      <GoogleOAuthProvider clientId={process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID!}>
-        <OAuth />
-      </GoogleOAuthProvider>
+      <OAuth />
     </QueryClientProvider>
   );
 };

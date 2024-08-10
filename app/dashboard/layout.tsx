34a0686ae890d7fbdc5ac4716d10fbc8d11f3b71
robuGdada <paudelronish@gmail.com>
2024-08-10 20:27:52 +0545

    dashboard queryclient init

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,21 +1,26 @@
 "use client";
 
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ThemeProvider } from "../component/theme-provider";
+
 export default function DashboardLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const queryClient = new QueryClient();
   return (
     <>
-      <ThemeProvider
-        attribute="class"
-        defaultTheme="system"
-        enableSystem
-        disableTransitionOnChange
-      >
-        <div className="flex h-screen ">{children}</div>
-      </ThemeProvider>
+      <QueryClientProvider client={queryClient}>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          <div className="flex h-screen ">{children}</div>
+        </ThemeProvider>
+      </QueryClientProvider>
     </>
   );
 }

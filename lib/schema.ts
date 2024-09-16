80396e4018af8c2ae50c5785077f5d3e0d5b3966
robuGdada <paudelronish@gmail.com>
2024-09-16 20:20:56 +0545

    schema updated

--- lib/schema.ts
+++ lib/schema.ts
@@ -3,12 +3,12 @@ import { z } from "zod";
 export const schema = z.object({
   productName: z.string().min(2, "This field is required").max(20),
   description: z.string().min(20, "Minimum 20 characters required"),
-  sellingPrice: z.number().min(1),
+  sellingPrice: z.number().optional(),
   crossedPrice: z.number().optional(),
-  quantity: z.number().min(1),
-  productImages: z.array(z.instanceof(File)),
+  quantity: z.number(),
+  productImages: z.array(z.instanceof(File)).optional(), // Make it optional if not required
   categoryId: z.string().min(1, "Category is required"),
-  variant: z.string().min(2).max(20),
+  variant: z.string().min(2).max(20).optional(),
   size: z.array(z.string().min(1)).nonempty("At least one size is required"),
   status: z.enum(["Active", "Draft"]),
 });
